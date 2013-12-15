eed1e5f007707fc6ce60005a72c579a1c8eb2b4b
rastating <intninety.nospam@gmail.com>
2013-12-15 14:19:50 +0000

    Removed version numbers from individual modules

--- modules/sprite_sheet.js
+++ modules/sprite_sheet.js
@@ -1,7 +1,5 @@
 // Copyright (C) 2013 rastating
 //
-// Version 0.0.3
-//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
