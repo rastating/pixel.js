086916983bb65a342f33e72ab52540bb9fabf656
rastating <intninety.nospam@gmail.com>
2013-12-22 15:33:01 +0000

    Updated build file to automatically remove duplicate copyright blocks

--- build/build.py
+++ build/build.py
@@ -21,10 +21,30 @@ modules = [
 ]
 
 compiled = ''
+processed_first_file = False
+copyright_block = """// Copyright (C) 2013 rastating
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/."""
+
 with open(root + '/pixel.js', 'w') as outfile:
     for name in modules:
         with open(root + '/../modules/' + name) as module:
-            compiled = compiled + module.read() + "\r\n"
+            if processed_first_file:
+                compiled = compiled + module.read().replace(copyright_block, "") + "\r\n"
+            else:
+                compiled = compiled + module.read() + "\r\n"
+            processed_first_file = True
     outfile.write(compiled)
     
 with open(root + '/pixel.min.js', 'w') as outfile:
